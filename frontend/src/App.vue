ad4a6d4f1600798b4bfbd84b20e9d64412c7655d
kabust <tulyulyuk91@gmail.com>
2025-05-26 22:55:56 +0200

    prepare init nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -12,17 +11,19 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/login">Contact Us</RouterLink>
       </span>
       <span>
+        <img alt="Instagram" src="@/assets/images/Instagram.svg">
         <RouterLink to="/login">@niff.wear</RouterLink>
       </span>
       <span>
-        <img alt="Niff logo" class="logo" src="@/assets/images/logo.svg" width="74px" height="24px"/>
+        <RouterLink to="/"><img alt="Niff logo" class="logo" src="@/assets/images/logo.svg" width="74px" height="24px"/></RouterLink>
       </span>
       <span>
+        <img alt="Cart" src="@/assets/images/Search.svg">
         <RouterLink to="/login">Search</RouterLink>
       </span>
       <span>
-        <RouterLink to="/login">Cart</RouterLink>
-        <RouterLink to="/login">Profile</RouterLink>
+        <RouterLink to="/login"><img alt="Cart" src="@/assets/images/Cart.svg"></RouterLink>
+        <RouterLink to="/login"><img alt="Profile" src="@/assets/images/Profile.svg"></RouterLink>
       </span>
     </nav>
 
@@ -39,15 +40,49 @@ header {
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  font-size: 16px;
+  /* text-align: center; */
+  margin-top: 0;
   display: flex;
+  /* align-items: center; */
+  border: 1px solid black;
+  border-top: 0;
 }
+
+nav a {
+  text-decoration: underline;
+  color: var(--color-text);
+  transition: 0.4s;
+  padding: 3px;
+}
+
+@media (hover: hover) {
+nav a:hover {
+    color: var(--color-text-hover);
+  }
+}
+
+@media (hover: hover) {
+nav a:active {
+    color: var(--color-text-muted);
+  }
+}
+
+nav span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px 54.5px;
+  border-right: 1px solid black;
+}
+
+nav span:last-child {
+  border-right: none;
+}
+
 
 </style>
