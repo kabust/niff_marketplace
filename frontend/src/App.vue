ea19bd73058413ef444996869d6353ae0a0bc9de
kabust <tulyulyuk91@gmail.com>
2025-05-28 23:12:29 +0200

    finish navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,40 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+import Instagram from '@/assets/images/Instagram.svg'
+import Search from '@/assets/images/Search.svg'
+import Cart from '@/assets/images/Cart.svg'
+import Profile from '@/assets/images/Profile.svg'
+
+
+function search() {
+  console.log("test");
+}
 </script>
 
 <template>
   <header>
     <nav>
-      <span>
+      <span class="info">
         <RouterLink to="/">Shop</RouterLink>
         <RouterLink to="/login">Become a Seller</RouterLink>
         <RouterLink to="/login">Contact Us</RouterLink>
       </span>
-      <span>
-        <img alt="Instagram" src="@/assets/images/Instagram.svg">
-        <RouterLink to="/login">@niff.wear</RouterLink>
+      <span class="inst">
+        <RouterLink to="/login">
+          <Instagram/>@niff.wear
+        </RouterLink>
       </span>
-      <span>
-        <RouterLink to="/"><img alt="Niff logo" class="logo" src="@/assets/images/logo.svg" width="74px" height="24px"/></RouterLink>
+      <span class="logo">
+        <RouterLink to="/"><img class="logo" src="./assets/images/logo.svg" width="72px" height="24px" /></RouterLink>
       </span>
-      <span>
-        <img alt="Cart" src="@/assets/images/Search.svg">
-        <RouterLink to="/login">Search</RouterLink>
+      <span class="search">
+        <Search @click="search" class="search-icon" />
+        <input type="search">
       </span>
-      <span>
-        <RouterLink to="/login"><img alt="Cart" src="@/assets/images/Cart.svg"></RouterLink>
-        <RouterLink to="/login"><img alt="Profile" src="@/assets/images/Profile.svg"></RouterLink>
+      <span class="user">
+        <RouterLink to="/login"><Cart/></RouterLink>
+        <RouterLink to="/login"><Profile/></RouterLink>
       </span>
     </nav>
 
@@ -38,17 +49,11 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-}
-
 nav {
-  width: 100%;
+  width: 100vw;
   font-size: 16px;
-  /* text-align: center; */
   margin-top: 0;
   display: flex;
-  /* align-items: center; */
   border: 1px solid black;
   border-top: 0;
 }
@@ -60,9 +65,35 @@ nav a {
   padding: 3px;
 }
 
+nav a, nav svg :deep(path) {
+    transition: color 0.3s ease, stroke 0.3s ease;
+}
+
 @media (hover: hover) {
-nav a:hover {
+nav a:hover, svg:hover :deep(path) {
     color: var(--color-text-hover);
+    stroke: var(--color-text-hover);
+  }
+}
+
+@media (hover: hover) {
+nav .inst a:hover svg :deep(path) {
+    color: var(--color-text-hover);
+    stroke: var(--color-text-hover);
+  }
+}
+
+@media (hover: hover) {
+nav a:active, svg:active :deep(path) {
+    color: var(--color-text-muted);
+    stroke: var(--color-text-muted);
+  }
+}
+
+@media (hover: hover) {
+nav .inst a:active svg :deep(path) {
+    color: var(--color-text-muted);
+    stroke: var(--color-text-muted);
   }
 }
 
@@ -74,15 +105,25 @@ nav a:active {
 
 nav span {
   display: flex;
+  flex: 1;
+
+  height: 72px;
   justify-content: center;
   align-items: center;
   padding: 24px 54.5px;
   border-right: 1px solid black;
+  gap: 24px;
 }
 
 nav span:last-child {
   border-right: none;
 }
 
+nav .info   { flex: 0 0 30%; }
+nav .inst, .inst a {display: flex; flex: 0 0 15%; gap: 8px;}
+nav .logo   { flex: 0 0 10%;}
+nav .search { flex: 0 0 30%; gap: 8px;}
+nav .user  { flex: 0 0 15%; }
+nav .search-icon {cursor: pointer};
 
 </style>
